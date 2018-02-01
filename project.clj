3edd8c9f0433e49eba2f7f7b01a48ff93b71a801
Steve Miner <steveminer@gmail.com>
2018-02-01 11:22:36 -0500

    init

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject com.velisco/dropshot "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/miner/dropshot"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
+
