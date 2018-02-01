77537744ffd9b23c088ee8ee5592edbd2e0d0f36
Steve Miner <steveminer@gmail.com>
2018-02-01 14:52:08 -0500

    ignore

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "https://github.com/miner/dropshot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.9"]
+                 [etaoin "0.2.4"]
+                 ])
+
 
